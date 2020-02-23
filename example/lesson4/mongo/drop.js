97595b4fa993724cf9df1d10ca8c68d3cd55bf16
chudo9991 <panfav40@gmail.com>
2020-02-23 19:01:37 +0300

    lesson 4 correct

--- example/lesson4/mongo/drop.js
+++ example/lesson4/mongo/drop.js
@@ -2,13 +2,13 @@ const MongoClient = require("mongodb").MongoClient;
    
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
-  
+ 
 mongoClient.connect(function(err, client){
      
     if(err) return console.log(err);
       
     const db = client.db("usersdb");
-    db.collection("users").drop(function(err, result){
+    db.collection("users").deleteOne({age: 32}, function(err, result){
               
         console.log(result);
         client.close();
